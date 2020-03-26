25c88129575074af23d406a015751d1fa742712f
amit <amitnh@gmail.com>
2020-03-26 13:20:48 +0200

    hanan's update

--- LAB1 task/aux_package.vhd
+++ LAB1 task/aux_package.vhd
@@ -9,7 +9,7 @@ package aux_package is
 	PORT (    cin : IN STD_LOGIC;
 			sel : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
 			 X,Y: IN STD_LOGIC_VECTOR (n-1 DOWNTO 0);
-		  result: OUT STD_LOGIC_VECTOR(n-1 downto 0));
+		  result: OUT STD_LOGIC_VECTOR(n downto 0));
 	end component;
   
   
--- LAB1 task/top.vhd
+++ LAB1 task/top.vhd
@@ -8,7 +8,7 @@ ENTITY top IS
   PORT (    cin : IN STD_LOGIC;
 			sel : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
 			 X,Y: IN STD_LOGIC_VECTOR (n-1 DOWNTO 0);
-		  result: OUT STD_LOGIC_VECTOR(n-1 downto 0));
+		  result: OUT STD_LOGIC_VECTOR(n downto 0));
 END top;
 ------------- complete the top Architecture code --------------
 ARCHITECTURE struct OF top IS
